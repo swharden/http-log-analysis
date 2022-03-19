5794e80bbd6b59db3b6fd4117c0527806998a565
Scott W Harden <swharden@gmail.com>
2022-03-19 17:13:46 -0400

    FTP: use top-level namespace

--- src/LogAnalysis/FTP.cs
+++ src/LogAnalysis/FTP.cs
@@ -1,52 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentFTP;
+﻿using FluentFTP;
 
-namespace LogAnalysis
+namespace LogAnalysis;
+
+internal static class FTP
 {
-    internal static class FTP
+    public static void DownloadLogs()
     {
-        public static void DownloadLogs()
+        FtpSecrets secrets = new();
+
+        using FtpClient client = new(secrets.Hostname, secrets.Username, secrets.Password);
+        client.EncryptionMode = FtpEncryptionMode.Explicit;
+        client.ValidateAnyCertificate = true;
+        client.Connect();
+
+        LogFiles.CreateLogFileFolder();
+
+        FtpListItem[] items = client.GetListing("/")
+            .Where(x => x.Name.EndsWith(".gz"))
+            .OrderBy(x => x.Name)
+            .ToArray();
+
+        foreach (FtpListItem item in items)
         {
-            FtpSecrets secrets = new();
+            string gzipFilePath = Path.Combine(LogFiles.LogFileFolder, item.Name);
 
-            using FtpClient client = new(secrets.Hostname, secrets.Username, secrets.Password);
-            client.EncryptionMode = FtpEncryptionMode.Explicit;
-            client.ValidateAnyCertificate = true;
-            client.Connect();
-
-            LogFiles.CreateLogFileFolder();
-
-            FtpListItem[] items = client.GetListing("/")
-                .Where(x => x.Name.EndsWith(".gz"))
-                .OrderBy(x => x.Name)
-                .ToArray();
-
-            foreach (FtpListItem item in items)
+            if (File.Exists(gzipFilePath))
             {
-                string gzipFilePath = Path.Combine(LogFiles.LogFileFolder, item.Name);
-
-                if (File.Exists(gzipFilePath))
+                long localFileSize = new FileInfo(gzipFilePath).Length;
+                if (item.Size == localFileSize)
                 {
-                    long localFileSize = new FileInfo(gzipFilePath).Length;
-                    if (item.Size == localFileSize)
-                    {
-                        Console.WriteLine($"Skipping: {item.Name} {item.Size}");
-                        continue;
-                    }
+                    Console.WriteLine($"Skipping: {item.Name} {item.Size}");
+                    continue;
                 }
-
-                Console.WriteLine($"Downloading: {item.Name}");
-                using FileStream fs = new(gzipFilePath, FileMode.CreateNew);
-                client.Download(fs, item.FullName);
-                fs.Close();
-
-                Console.WriteLine($"Decompressing: {item.Name}");
-                LogFiles.Decompress(gzipFilePath);
             }
+
+            Console.WriteLine($"Downloading: {item.Name}");
+            using FileStream fs = new(gzipFilePath, FileMode.CreateNew);
+            client.Download(fs, item.FullName);
+            fs.Close();
+
+            Console.WriteLine($"Decompressing: {item.Name}");
+            LogFiles.Decompress(gzipFilePath);
         }
     }
 }
